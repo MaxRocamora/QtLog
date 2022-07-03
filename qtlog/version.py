115ac5b7196e0ac5ce1085f301e217b5f53c5705
Maximilliano Rocamora <maxirocamora@gmail.com>
2022-07-03 18:02:46 -0300

    1.0.1

--- qtlog/version.py
+++ qtlog/version.py
@@ -1,10 +1,11 @@
 # ----------------------------------------------------------------------------------------
 # 1.0.0 05/2022 - Initial Release
+# 1.0.1 07/2022 - Fixed Import paths
 # ----------------------------------------------------------------------------------------
 
 VERSION_MAJOR = 1
 VERSION_MINOR = 0
-VERSION_PATCH = 0
+VERSION_PATCH = 1
 
 version = f'{VERSION_MAJOR}.{VERSION_MINOR}.{VERSION_PATCH}'
 
