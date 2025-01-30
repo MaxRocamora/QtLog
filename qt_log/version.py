f5a9d9cd2f91b69f9dad8c971aefb37751bc2c73
Maximilliano Rocamora <maxirocamora@gmail.com>
2025-01-30 17:03:56 -0300

    Bump version to 1.1.2 and fix font monospace property on PySide6

--- qt_log/version.py
+++ qt_log/version.py
@@ -2,11 +2,12 @@
 # 1.0.0 05/2022 - Initial Release
 # 1.0.1 07/2022 - Fixed Import paths
 # 1.1.1 01/2025 - Added PySide6 imports, Ruff Format, Poetry
+# 1.1.2 02/2025 - Fixed font monospace property on PySide6
 # ----------------------------------------------------------------------------------------
 
 VERSION_MAJOR = 1
 VERSION_MINOR = 1
-VERSION_PATCH = 1
+VERSION_PATCH = 2
 
 version = f'{VERSION_MAJOR}.{VERSION_MINOR}.{VERSION_PATCH}'
 
