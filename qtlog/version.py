2a02483551d71c50862dff08dc9424b28ff4afdd
Maximilliano Rocamora <maxirocamora@gmail.com>
2025-01-30 15:00:24 -0300

    Bump version to 1.1.0 and add PySide6 imports, Ruff format, and Poetry support

--- qtlog/version.py
+++ qtlog/version.py
@@ -1,11 +1,12 @@
 # ----------------------------------------------------------------------------------------
 # 1.0.0 05/2022 - Initial Release
 # 1.0.1 07/2022 - Fixed Import paths
+# 1.1.0 01/2025 - Added PySide6 imports, Ruff Format, Poetry
 # ----------------------------------------------------------------------------------------
 
 VERSION_MAJOR = 1
-VERSION_MINOR = 0
-VERSION_PATCH = 1
+VERSION_MINOR = 1
+VERSION_PATCH = 0
 
 version = f'{VERSION_MAJOR}.{VERSION_MINOR}.{VERSION_PATCH}'
 
