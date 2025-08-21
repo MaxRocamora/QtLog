cf1a21888f39ea57874ea65fe3f1b0e2c20e6917
Maximilliano Rocamora <maxirocamora@gmail.com>
2025-08-21 12:27:31 -0300

    Bump version to 1.1.3 and update changelog

--- qt_log/version.py
+++ qt_log/version.py
@@ -3,11 +3,12 @@
 # 1.0.1 07/2022 - Fixed Import paths
 # 1.1.1 01/2025 - Added PySide6 imports, Ruff Format, Poetry
 # 1.1.2 02/2025 - Fixed font monospace property on PySide6
+# 1.1.3 08/2025 - Fixed poetry python requirements
 # ----------------------------------------------------------------------------------------
 
 VERSION_MAJOR = 1
 VERSION_MINOR = 1
-VERSION_PATCH = 2
+VERSION_PATCH = 3
 
 version = f'{VERSION_MAJOR}.{VERSION_MINOR}.{VERSION_PATCH}'
 
